53ea521a734cc15878a085b2d7ef66fc7176e228
davifelinto <40410923+davifelinto@users.noreply.github.com>
2023-03-29 21:28:43 -0300

    Update exercicio2_aula2.js

--- Exercicios/exercicio2_aula2.js
+++ Exercicios/exercicio2_aula2.js
@@ -1,37 +1,31 @@
 // Multiplicação de matrizes
 
-const A = [ [1,2],[3,4] ]
-const B = [ [1,2],[3,4] ]
-const C = [ [],[] ]
+const A = [[1,2],[3,4]]
+const B = [[1,2],[3,4]]
 
-if (A.length == B[0].length){
+const linhasA = A.length
+const colunasA = A[0].length
+const linhasB = B.length
+const colunasB = B[0].length
 
-    console.log('AxB=C:')
-    for(let i = 0; i < A.length; i++){
-        let linha = ''
-        for(let j = 0; j < B.length; j++){
-            C[i][j] = 0
-            for(let k = 0; k < B[j].length; k++){
-                C[i][j] += A[i][k]*B[k][i]
-            }
-            linha += C[i][j] + ' '
-        }
-        console.log(linha)
+if (colunasA == linhasB) {
+  const C = new Array(linhasA)
+  for (let i = 0; i < linhasA; i++) {
+    C[i] = new Array(colunasB).fill(0)
+  }
+  
+  for (let i = 0; i < linhasA; i++) {
+    for (let j = 0; j < colunasB; j++) {
+      for (let k = 0; k < colunasA; k++) {
+        C[i][j] += A[i][k] * B[k][j]
+      }
     }
+  }
 
+  console.log("AxB=C:")
+  for (let i = 0; i < linhasA; i++) {
+    console.log(C[i])
+  }
+} else {
+  console.log("As matrizes não podem ser multiplicadas.")
 }
-else{
-    console.log("Matrizes não multiplicáveis")
-}
-
-// for(i = 0; i < linhaA; i++) {
-//     for(j = 0; j < colunaB; j++) {
-//         matrizC[i][j] = 0;
-//         for(x = 0; x < linhaB; x++) {
-//             aux +=  matrizA[i][x] * matrizB[x][j];
-//         }
-
-//         matrizC[i][j] = aux;
-//         aux = 0;
-//     }
-// }
